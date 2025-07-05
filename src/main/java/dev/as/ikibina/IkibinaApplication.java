a52d38fb5ae3c311b58a5ec57786f2610964274c
Tuyishime Anne <atuyishime28@gmail.com>
2025-07-06 01:21:23 +0300

    initial commit

---
+++ src/main/java/dev/as/ikibina/IkibinaApplication.java
@@ -0,0 +1,13 @@
+package dev.as.ikibina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IkibinaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IkibinaApplication.class, args);
+    }
+
+}
